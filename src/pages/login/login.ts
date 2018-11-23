121388ec36b80577720d988e93517b0739f84ded
Jesse Cheung <jesse.cheung67@gmail.com>
2018-11-22 21:41:22 -0800

    Created a login/register page

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { User } from "../../models/user";
+import { AngularFireAuth} from 'angularfire2/auth'; 
+
 /**
  * Generated class for the LoginPage page.
  *
@@ -18,15 +20,27 @@ export class LoginPage {
   user = {} as User;
 
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    private afAuth: AngularFireAuth,
+    public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
   }
-  login(){
-
+  async login(user: User){
+    try { 
+      const result = this.afAuth.auth.signInWithEmailAndPassword(user.email, user.password);
+      console.log(result);
+      if(result){
+      this.navCtrl.push('SettingspagePage');
+      }
+    }
+  catch(e) {
+    console.error(e);
   }
+}
+
 
   register(){
    this.navCtrl.push('RegisterPage'); 
--- src/pages/settingspage/settingspage.ts
+++ src/pages/settingspage/settingspage.ts
@@ -1,21 +1,42 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { HomePage } from '../home/home'
+        import { Component } from '@angular/core';
+        import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
+        import { HomePage } from '../home/home'
+        import { AngularFireAuth } from 'angularfire2/auth';
 
-@IonicPage()
-@Component({
-  selector: 'page-settingspage',
-  templateUrl: 'settingspage.html',
-})
-export class SettingspagePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+        @IonicPage()
+        @Component({
+          selector: 'page-settingspage',
+          templateUrl: 'settingspage.html',
+        })
+        export class SettingspagePage {
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SettingspagePage');
-  }
-openHome(){
-  this.navCtrl.push(HomePage);
-}
-}
+          constructor(
+            private afAuth: AngularFireAuth, private toast: ToastController,
+            public navCtrl: NavController, public navParams: NavParams) {
+            }
+
+          ionViewDidLoad() {
+            this.afAuth.authState.subscribe(data => {
+              if(data && data.email && data && data.uid) {
+              this.toast.create({
+                message: 'Welcome to the DATABASE, ${data.email}',
+                duration: 3000,
+              }).present();
+            }
+            else {
+              this.navCtrl.setRoot('Login')
+              this.toast.create({
+                message: 'Incorrect email or password',
+                duration: 3000
+              }).present();
+            }
+          })
+        }
+          
+
+            openHome(){
+              this.navCtrl.push(HomePage);
+          }
+          
+        }
